67976137a48c8d9f41eab8bea984eb7e551841a3
zrtch <1160639597@qq.com>
2020-03-19 13:06:59 +0800

    项目基础架构

--- src/main.js
+++ src/main.js
@@ -1,10 +1,37 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue"
+import router from "./router"
+import axios from "axios"
+import VueAxios from "vue-axios"
+import App from "./App.vue"
+// import env from "./env"
 
+//mock开关
+const mock = true
+if (mock) {
+  require("./mock/api")
+}
+//根据前端的跨域方式做调整 /api是代理的方式
+axios.defaults.baseURL =
+  " https://www.easy-mock.com/mock/5e72fc39e09fec4e500b40a8/api"
+axios.defaults.timeout = 8000
+// 根据环境变量获取不同的请求地址
+// axios.defaults.baseURL = env.baseURL
+//接口错误拦截
+axios.interceptors.response.use(function(response) {
+  let res = response.data
+  if (res.status == 0) {
+    return res.data
+  } else if (res.status == 10) {
+    window.location.href = "/#/login"
+  } else {
+    alert(res.msg)
+  }
+})
+
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app")
